c7641503804bbe539d702616fd96dc552e2e48a0
springbottom <joshua.z.lin@berkeley.edu>
2020-01-01 10:57:31 -0800

    Initial Commit

---
+++ Chess Notes/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Chess Notes
+//
+//  Created by Joshua Lin on 1/1/20.
+//  Copyright © 2020 Joshua Lin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
